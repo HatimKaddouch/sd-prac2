49d8c200e28e499a7ee827e0ab4dfe3039245b40
Hatim Kaddouch <hatim.kaddouch@estudiants.urv.cat>
2019-06-14 14:22:40 +0200

    Version 2.0

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import pywren_ibm_cloud as pywren
 import pika
+import random
+import json
 
 def main():
     args = []
@@ -20,7 +22,7 @@ def main():
     connection = pika.BlockingConnection(params)
     channel = connection.channel() # start a channel
 
-    channel.exchange_declare(exchange='publish_subscribe')
+    channel.exchange_declare(exchange='publish_subscribe', exchange_type='fanout')
 
     pw.call_async(my_function_leader, N)
 
@@ -36,25 +38,30 @@ def main():
     results = pw.get_result()
 
     print(results)
+
+    channel.exchange_delete(exchange='publish_subscribe', if_unused=False)
+    connection.close()
+
     exit(0)
 
 def my_function_leader(N): #N = num maps
+    print("Yo soy el lider")
     messages = 0
     maps = N
     petitions = []
     i = 0
 
-    def callback(ch, method, properties, body):
+    def callback_1(ch, method, properties, body):
         nonlocal petitions
         nonlocal messages
         nonlocal maps
         nonlocal i
-        data = body.decode('utf-8')
+        data = body.decode('ascii')
+        print("Peticion recibida de "+data)
         petitions.append(data)
         messages += 1
         if messages == maps - i:
             ch.stop_consuming()
-            messages = 0
 
     pw_config = json.loads(os.environ.get('PYWREN_CONFIG', ''))
     params = pika.URLParameters(pw_config['rabbitmq']['amqp_url'])
@@ -65,21 +72,24 @@ def my_function_leader(N): #N = num maps
 
     for i in range(maps):
         petitions = []
+        messages = 0
 
-        channel.basic_consume(callback,
+        channel.basic_consume(callback_1,
                               queue='leader',
                               no_ack=True)
         channel.start_consuming()
 
         slave = random.choice(petitions)
         channel.basic_publish(exchange='',
-                          routing_key='slave'+str(slave),
+                          routing_key='slave'+slave,
                           body='write_allowed')
+        print("Permiso enviado a "+slave)
 
     channel.queue_delete(queue='leader')
     connection.close()
 
 def my_function_slave(id, N): #N = num maps
+    print("Yo soy el esclavo "+str(id))
     randoms = []
     messages = 0
     maps = N
@@ -91,22 +101,27 @@ def my_function_slave(id, N): #N = num maps
         nonlocal maps
         nonlocal i
         nonlocal write
-        data = body.decode('utf-8')
-        if data == 'write_allowed':
+
+        data = body.decode('ascii')
+        if data == "write_allowed":
+            print("Permiso recibido del lider")
             rand = random.randint(0,500)
-            channel.basic_publish(exchange='publish_subscribe',
+            ch.basic_publish(exchange='publish_subscribe',
                               routing_key='',
                               body=str(rand))
+
+            print("Aleatorio enviado "+str(rand))
             write = True
         else:
-            randoms.append(int(data))
+            randoms.append(data)
+            print("Aleatorio recibido "+data)
             messages += 1
-            if messages == maps:
-                ch.stop_consuming()
-            elif not write:
-                channel.basic_publish(exchange='',
+            if not write:
+                ch.basic_publish(exchange='',
                                   routing_key='leader',
-                                  body=str(id))
+                                  body=str(i))
+        if messages == maps:
+            ch.stop_consuming()
 
     pw_config = json.loads(os.environ.get('PYWREN_CONFIG', ''))
     params = pika.URLParameters(pw_config['rabbitmq']['amqp_url'])
